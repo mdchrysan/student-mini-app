07edced109fe5a446c48673d422f0cd01955b58c
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2020-11-11 18:49:10 +0700

    Succesfully adding students3 properties

--- src/app/week8/students3/form/form.page.ts
+++ src/app/week8/students3/form/form.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Student2 } from '../../students2/students2.model';
+import { Students3Service } from '../../students3.service';
 
 @Component({
   selector: 'app-form',
@@ -7,9 +11,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormPage implements OnInit {
 
-  constructor() { }
+  constructor(private students3Srv: Students3Service, private router: Router) { }
 
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm){
+    console.log(form);
+    
+    const student: Student2 = {
+      nim: form.value.nim,
+      nama: form.value.nama,
+      prodi: form.value.prodi,
+    };
+    this.students3Srv.addStudent(student);
+    this.router.navigateByUrl('/students3');
+  }
 }
--- src/app/week8/students3/students3.page.ts
+++ src/app/week8/students3/students3.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Student2 } from '../students2/students2.model';
+import { Students3Service } from '../students3.service';
 
 @Component({
   selector: 'app-students3',
@@ -6,10 +9,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./students3.page.scss'],
 })
 export class Students3Page implements OnInit {
-
-  constructor() { }
+  students: Student2[];
+  private studentsSub: Subscription;
+  constructor(private students3Srv: Students3Service) { }
 
   ngOnInit() {
+    this.studentsSub = this.students3Srv.getAllStudents().subscribe(students => {
+      this.students = students;
+    });
   }
 
+  ngOnDestroy(){
+    if(this.studentsSub){
+      this.studentsSub.unsubscribe();
+    }
+  }
 }
