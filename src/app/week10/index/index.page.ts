346fdf7052a1a1fb2ac320b177ed5a2a4b730b38
Maria Dimansi Chrysandini <chrysan.scorpio@gmail.com>
2020-11-14 23:56:49 +0700

    Editing index html and ts.

--- src/app/week10/index/index.page.ts
+++ src/app/week10/index/index.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MahasiswaService } from '../mahasiswa.service';
+import { take, map, delay, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-index',
@@ -6,10 +8,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.page.scss'],
 })
 export class IndexPage implements OnInit {
-
-  constructor() { }
+  mahasiswa: any;
+  constructor(private mhsSrv: MahasiswaService) { }
 
   ngOnInit() {
+    this.mhsSrv.getAll().snapshotChanges().pipe(
+      map(changes => changes.map(c => ({key: c.payload.key , ...c.payload.val()})))
+    ).subscribe(data=>{
+      this.mahasiswa = data;
+      console.log(data);
+    })
   }
 
+  delete(event, key){
+    console.log(key);
+    this.mhsSrv.delete(key).then(res=>{
+      console.log(res);
+    })
+  }
 }
